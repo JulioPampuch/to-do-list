4d33c43b7c0a116b4b22ecf2a879a10b1fcbd6bc
Julio Pampuch <juliopampuch@gmail.com>
2023-02-02 23:46:25 -0300

    testing database

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,18 +2,36 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  console.log('Test')
-  res.send('test')
+const Checklist = require('./models/checklist')
+
+router.get('/', async (req, res) => {
+  try {
+    let checklists = await Checklist.find({})
+    res.status(200).json(checklists)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 })
 
-router.get('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`id: ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id)
+    res.status(200).json(checklist)
+  } catch (err) {
+    res.status(422).json(err)
+  }
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body)
+router.post('/', async (req, res) => {
+  let { name } = req.body
+
+  try {
+    let checklist = await Checklist.create({ name })
+    res.status(200).json(checklist)
+  } catch (error) {
+    res.status(422).json(error)
+  }
+
   res.status(200).send(req.body)
 })
 
