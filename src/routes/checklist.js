1090005f48231cbc9999442bb0deca88c90d2cb9
Julio Pampuch <juliopampuch@gmail.com>
2023-02-07 11:36:17 -0300

    learning get, put, post, and delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 const Checklist = require('./models/checklist')
 
+// Pega todas as tasks do checklist
 router.get('/', async (req, res) => {
   try {
     let checklists = await Checklist.find({})
@@ -13,6 +14,7 @@ router.get('/', async (req, res) => {
   }
 })
 
+// Pesquisa a task pelo id na url
 router.get('/:id', async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id)
@@ -22,12 +24,13 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+// Cria uma tarefa
 router.post('/', async (req, res) => {
   let { name } = req.body
   // re.body['name']
 
   try {
-    let checklist = await Checklist.create({ name })
+    let checklist = await Checklist.create({ "name": name })
     res.status(200).json(checklist)
   } catch (error) {
     res.status(422).json(error)
@@ -36,9 +39,30 @@ router.post('/', async (req, res) => {
   res.status(200).send(req.body)
 })
 
-router.put('/:id', (req, res) => {
-  console.log(req.params.id)
+// Atualiza uma task
+router.put('/:id', async (req, res) => {
+  let { name } = req.body
+
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true })
+    res.status(200).json(checklist)
+  } catch (err) {
+    res.status(422).json(err)
+  }
   res.send(` put id: ${req.params.id}`)
+})
+
+// Deleta uma task
+router.delete('/:id', async (req, res) => {
+  let { name } = req.body
+
+  try {
+    let checklist = await Checklist.findByIdAndDelete(req.params.id, { name })
+    res.status(200).json(checklist)
+  } catch (err) {
+    res.status(422).json(err)
+  }
+
 })
 
 module.exports = router
